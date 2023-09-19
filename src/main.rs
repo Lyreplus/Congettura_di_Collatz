cb3ee8729f8eb45e970499679d5342e2b88574ac
MarognaLorenzo <lmarogna02@gmail.com>
2023-09-19 13:52:09 +0200

    long integer + best result printing

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
 fn main() {
     println!("Hello, world!");
     let mut x = 6;
-    let mut result: i32;
+    let mut result: i64;
+    let mut best_result = 0;
+    let mut best_num = 1;
     while true {
-        println!("{}", x);
         result = congettura(x);
-        println!("{}", result);
+        if result > best_result {best_result = result; best_num = x; }
+        println!("br: {} with num {}... {} -> {} steps before getting to 1",best_result, best_num, x, result);
         x = x+1;
     }
 }
 
-fn congettura(mut n: i32) -> i32{
+fn congettura(mut n: i64) -> i64{
+    let mut cont = 0;
     while n>1 {
         if n%2!=0 {
             n = 3*n+1;
         }else{
             n = n/2;
         }
-        //println!("{}", n);
+        cont += 1;
     }
-    n
+    cont
 }
